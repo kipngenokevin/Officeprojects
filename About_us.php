b077179efe3cb8af752e3363f6fad4a2ce1d008c
Kevin Kipngeno <35192411+kipngenokevin@users.noreply.github.com>
2021-04-19 09:49:24 +0300

    About_us update

--- About_us.php
+++ About_us.php
@@ -70,7 +70,7 @@
     box-sizing:border-box;
   }
   .about_us{
-    background-image:url("pictures/about_us_1.jpg");
+    background-color:#DDDDDD;
   }
   .containr {
     padding: 64px;
@@ -173,7 +173,7 @@
     <div class="container containr">
       <div class="row">
         <div class="column-66">
-          <h1 class="xlarge-font"><b>The App</b></h1>
+          <h1 class="xlarge-font" id="vision_section"><b>Vision</b></h1>
           <h1 class="large-font" style="color:MediumSeaGreen;"><b>Why buy it?</b></h1>
           <p><span style="font-size:36px">Take photos like a pro.</span> You should buy this app because lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
           <button class="button_about">Download Application</button>
